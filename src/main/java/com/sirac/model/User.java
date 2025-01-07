692cd017ce303e38e7a41c74bf36dc295e3ddd94
Siraç Petmezçiler <Sirac_ptmzclr@hotmail.com>
2025-01-07 04:18:13 +0300

    User Model update

---
+++ src/main/java/com/sirac/model/User.java
@@ -0,0 +1,26 @@
+package com.sirac.model;
+
+import com.fasterxml.jackson.databind.ser.Serializers;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseEntity {
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "nickname")
+    private String nickname;
+}
